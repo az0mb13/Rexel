fbd9b7dfe6a02ae5e04d24fae6426605b9904e9f
Aditya Dixit <qwertyuiop.dixit3@gmail.com>
2020-08-22 22:47:40 +0530

    Fixed arrays with IFS

--- rexel.sh
+++ rexel.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+SAVEIFS=$IFS
+IFS=$'\n'
 dir_url=$1
 
+declare -A dir_list
+
 curl_out=`curl -s $1 > curl_outfile`
-directories=($(grep -o '".*\/"' curl_outfile))
+dir_names=($(grep -o '">.*<' curl_outfile))
+dir_urls=($(grep -o '".*\/"' curl_outfile))
 
-for t in ${directories[@]}; do
-    echo $dir_url${t:1:-1}
+for t in ${dir_names[@]}; do
+    #echo $dir_url${t:1:-1}
+    echo ${t:2:-2}
 done
-#echo $result
-
