bd413e0dc72f0ecbe2b073deda46bc04aac16acd
Aditya Dixit <qwertyuiop.dixit3@gmail.com>
2020-08-23 11:39:13 +0530

    completed adding associative array

--- rexel.sh
+++ rexel.sh
@@ -10,7 +10,11 @@ curl_out=`curl -s $1 > curl_outfile`
 dir_names=($(grep -o '">.*<' curl_outfile))
 dir_urls=($(grep -o '".*\/"' curl_outfile))
 
-for t in ${dir_names[@]}; do
-    #echo $dir_url${t:1:-1}
-    echo ${t:2:-2}
+
+for ((i=0;i<${#dir_names[@]}; i++)); do
+    dir_list[${dir_names[$i]:2:-2}]=$dir_url${dir_urls[$i]:1:-1}
+done
+
+for key in "${!dir_list[@]}"; do
+    echo "$key => ${dir_list[$key]}";
 done
